414fac236f80f1c0c8fc43f00a8250c3aa179ef7
Jay Patel <jay3686@gmail.com>
2016-04-22 01:48:31 -0400

    add related artists

--- server.js
+++ server.js
@@ -15,3 +15,41 @@ var getFromApi = function(endpoint, args) {
   });
   return emitter;
 };
+
+
+var app = express();
+app.use(express.static('public'));
+
+app.get('/search/:name', function(req, res) {
+  var searchReq = getFromApi('search', {
+    q: req.params.name,
+    limit: 1,
+    type: 'artist'
+  });
+
+  searchReq.on('end', function(item) {
+    var artist = item.artists.items[0];
+    if(artist !== undefined) {
+      var relatedArtists = getFromApi('artists/' + artist.id + '/related-artists');
+
+      relatedArtists.on('end', function(related) {
+        artist.related = related.artists;
+        console.log('artist:', artist);
+        res.json(artist);
+      });
+
+      relatedArtists.on('error', function() {
+        res.sendStatus(404);
+      });
+    }
+
+  });
+
+  searchReq.on('error', function(code) {
+    res.sendStatus(code);
+  });
+
+
+});
+
+app.listen(8080);
